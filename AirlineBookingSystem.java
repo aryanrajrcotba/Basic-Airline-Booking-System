259347533852b2d450ecee0a038dbb36129d5eb9
Aryan Raj <158842298+aryanrajrcotba@users.noreply.github.com>
2024-12-19 20:33:39 +0530

    Update AirlineBookingSystem.java

--- AirlineBookingSystem.java
+++ AirlineBookingSystem.java
@@ -7,7 +7,7 @@ public class AirlineBookingSystem {
     // Database connection variables
     private static final String DB_URL = "jdbc:mysql://localhost:3306/airline_db";
     private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "Patna@1234";
+    private static final String DB_PASSWORD = "rootpass";
 
     public static void main(String[] args) {
         new AirlineBookingSystem().createUI();
